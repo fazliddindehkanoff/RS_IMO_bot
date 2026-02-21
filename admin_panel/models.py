5f02ea27840562173a1fbc160e36e8f3e7b33770
Fazliddin Dehqonov <fazliddindehqonov@users.noreply.github.com>
2026-02-21 23:19:57 +0500

    bug fix

--- admin_panel/models.py
+++ admin_panel/models.py
@@ -61,7 +61,7 @@ class Student(models.Model):
         ('Qoraqalpog\'iston', 'Qoraqalpog\'iston')
     ]
     GRADE_CHOICES = [
-        (0, 'Gooo'),
+        (1, 'Gooo'),
         (5, '5-sinf'),
         (6, '6-sinf'),
         (7, '7-sinf'),
@@ -362,7 +362,7 @@ class RegistrationSource(models.Model):
 class Exam(models.Model):
     """Exam/test definition."""
     GRADE_CHOICES = [
-        (0, 'Gooo'),
+        (1, 'Gooo'),
         (5, '5-sinf'),
         (6, '6-sinf'),
         (7, '7-sinf'),
@@ -609,7 +609,7 @@ class BotState(models.Model):
 class Test(models.Model):
     """Test definition with grade-based separation."""
     GRADE_CHOICES = [
-        (0, 'Gooo'),
+        (1, 'Gooo'),
         (5, '5-sinf'),
         (6, '6-sinf'),
         (7, '7-sinf'),
